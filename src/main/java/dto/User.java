2a69bf017c380c610c410471e376e61c8c27517c
Swapnil Adhikari <161732970+Swapnil434@users.noreply.github.com>
2025-05-12 08:59:47 +0545

    Update User.java

--- src/main/java/dto/User.java
+++ src/main/java/dto/User.java
@@ -1,27 +1,34 @@
 package dto;
 
-public class User {
+import java.util.List;
 
+public class User {
+    private int id;
     private String username;
     private String email;
     private String checkin;
     private String checkout;
     private String roomtype;
-
+    private List<String> foodItems;
 
     public User() {
     }
 
-    public User( String username, String email, String checkin, String checkout, String roomtype ) {
-
+    public User(String username, String email, String checkin, String checkout, String roomtype) {
         this.username = username;
         this.email = email;
         this.checkin = checkin;
         this.checkout = checkout;
         this.roomtype = roomtype;
-
     }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
 
     public String getUsername() {
         return username;
@@ -61,5 +68,13 @@ public class User {
 
     public void setRoomtype(String roomtype) {
         this.roomtype = roomtype;
+    }
+
+    public List<String> getFoodItems() {
+        return foodItems;
+    }
+
+    public void setFoodItems(List<String> foodItems) {
+        this.foodItems = foodItems;
     }
 }
